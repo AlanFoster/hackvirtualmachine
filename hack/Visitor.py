8d5ca34cfdadb97d10a5cac4233b7ebea3972aef
Alan Foster <alan@alanfoster.me>
2019-03-31 19:15:33 +0100

    Add support for push vm instructions

--- hack/Visitor.py
+++ hack/Visitor.py
@@ -9,7 +9,7 @@ class Visitor(VMVisitor):
 
     # Visit a parse tree produced by VMParser#statements.
     def visitStatements(self, ctx: VMParser.StatementsContext):
-        return '\n'.join([self.visit(child) for child in ctx.getChildren()])
+        return '\n'.join([self.visit(child) for child in ctx.getChildren()]) + '\n'
 
     # Visit a parse tree produced by VMParser#statement.
     def visitStatement(self, ctx: VMParser.StatementContext):
@@ -17,20 +17,64 @@ class Visitor(VMVisitor):
 
     # Visit a parse tree produced by VMParser#push.
     def visitPush(self, ctx: VMParser.PushContext):
-        return "push " + self.visit(ctx.segment()) + " " + ctx.INT().getText()
+        segment = ctx.segment().getText()
 
-    # Visit a parse tree produced by VMParser#pop.
-    def visitPop(self, ctx: VMParser.PopContext):
-        return "pop " + self.visit(ctx.segment()) + " " + ctx.INT().getText()
+        # Mapping human readable virtual machine names to the hack assembly labels
+        hack_segment_names = {
+            'local': 'LCL',
+            'argument': 'ARG',
+            'this': 'THIS',
+            'that': 'THAT'
+        }
+
+        if segment == 'constant':
+            value = ctx.INT().getText()
+            return '\n'.join([
+                # Set constant in stack pointer
+                f"@{value}",
+                "D=A",
+                "@SP",
+                "M=D",
+
+                # Increment stack, sp++
+                "@SP",
+                "M=M+1",
+            ])
+        elif segment in hack_segment_names:
+            i = ctx.INT().getText()
+
+            return '\n'.join([
+                # Calculating addr, where addr = LCL + i
+                f"@{hack_segment_names[segment]}",  # Load the value stored in LCL
+                "D=M",
+                f"@{i}",  # Load i
+                "D=D+M",  # D is now addr = LCL + i
+
+                "D=M",  # Load the value stored within addr, i.e. *addr
+                "@SP",  # Fetch the segment pointer
+                "A=M",  # Load *sp into the address register
+                "M=D",  # *sp = *addr
+
+                # Increment stack, sp++
+                "@SP",
+                "M=M+1"
+            ])
+        else:
+            raise ValueError(f'unexpected segment value: "{segment}"')
+
+
+# Visit a parse tree produced by VMParser#pop.
+def visitPop(self, ctx: VMParser.PopContext):
+    return "pop " + self.visit(ctx.segment()) + " " + ctx.INT().getText()
 
     # Visit a parse tree produced by VMParser#arithmetic.
-    def visitArithmetic(self, ctx: VMParser.ArithmeticContext):
-        return ctx.getText()
+
+
+def visitArithmetic(self, ctx: VMParser.ArithmeticContext):
+    return ctx.getText()
 
     # Visit a parse tree produced by VMParser#logical.
-    def visitLogical(self, ctx: VMParser.LogicalContext):
-        return ctx.getText()
 
-    # Visit a parse tree produced by VMParser#stack.
-    def visitSegment(self, ctx: VMParser.SegmentContext):
-        return ctx.getText()
+
+def visitLogical(self, ctx: VMParser.LogicalContext):
+    return ctx.getText()
--- test/test_hack.py
+++ test/test_hack.py
@@ -6,31 +6,55 @@ def convert(vm_input):
     return VmToHack.convert(antlr4.InputStream(vm_input))
 
 
+def combine(instructions):
+    return '\n'.join(instructions) + '\n'
+
+
 def test_push_constant():
-    vm_input = \
-        "push constant 10\n" \
-        "pop local 0\n" \
-        "push constant 21\n" \
-        "push constant 22\n" \
-        "pop argument 2\n" \
-        "pop argument 1\n" \
-        "push constant 36\n" \
-        "pop this 6\n" \
-        "push constant 42\n" \
-        "push constant 45\n" \
-        "pop that 5\n" \
-        "pop that 2\n" \
-        "push constant 510\n" \
-        "pop temp 6\n" \
-        "push local 0\n" \
-        "push that 5\n" \
-        "add\n" \
-        "push argument 1\n" \
-        "sub\n" \
-        "push this 6\n" \
-        "push this 6\n" \
-        "add\n" \
-        "sub\n" \
-        "push temp 6\n" \
-        "add"
-    assert convert(vm_input) == vm_input
+    vm_input = "push constant 1337"
+    expected = combine([
+        "@1337",
+        "D=A",
+        "@SP",
+        "M=D",
+        "@SP",
+        "M=M+1",
+    ])
+
+    assert convert(vm_input) == expected
+
+
+def test_push_local():
+    vm_input = "push local 6"
+    expected = combine([
+        "@LCL",
+        "D=M",
+        "@6",
+        "D=D+M",
+        "D=M",
+        "@SP",
+        "A=M",
+        "M=D",
+        "@SP",
+        "M=M+1",
+    ])
+
+    assert convert(vm_input) == expected
+
+
+def test_push_argument():
+    vm_input = "push argument 12"
+    expected = combine([
+        "@ARG",
+        "D=M",
+        "@12",
+        "D=D+M",
+        "D=M",
+        "@SP",
+        "A=M",
+        "M=D",
+        "@SP",
+        "M=M+1",
+    ])
+
+    assert convert(vm_input) == expected
