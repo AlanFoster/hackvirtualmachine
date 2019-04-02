bfdaa08a217336a452c7d0329d1d031ebba2b3c5
Alan Foster <alan@alanfoster.me>
2019-04-02 19:24:18 +0100

    Add black and linting tools

--- hack/Visitor.py
+++ hack/Visitor.py
@@ -23,7 +23,7 @@ class Visitor(VMVisitor):
             # "M=A",
         ]
         instructions = [self.visit(child) for child in ctx.getChildren()]
-        return '\n'.join(preamble + instructions) + '\n'
+        return "\n".join(preamble + instructions) + "\n"
 
     # Visit a parse tree produced by VMParser#statement.
     def visitStatement(self, ctx: VMParser.StatementContext):
@@ -35,49 +35,50 @@ class Visitor(VMVisitor):
 
         # Mapping human readable virtual machine names to the hack assembly labels
         hack_segment_names = {
-            'local': 'LCL',
-            'argument': 'ARG',
-            'this': 'THIS',
-            'that': 'THAT'
+            "local": "LCL",
+            "argument": "ARG",
+            "this": "THIS",
+            "that": "THAT",
         }
 
-        if segment == 'constant':
+        if segment == "constant":
             value = ctx.INT().getText()
-            return '\n'.join([
-                f"// push constant {value}",
-
-                # Set constant in stack pointer
-                f"@{value}",
-                "D=A",
-                "@SP",
-                "A=M",
-                "M=D",
-
-                # Increment stack, sp++
-                "@SP",
-                "M=M+1",
-            ])
-        elif segment == 'static':
+            return "\n".join(
+                [
+                    f"// push constant {value}",
+                    # Set constant in stack pointer
+                    f"@{value}",
+                    "D=A",
+                    "@SP",
+                    "A=M",
+                    "M=D",
+                    # Increment stack, sp++
+                    "@SP",
+                    "M=M+1",
+                ]
+            )
+        elif segment == "static":
             i = int(ctx.INT().getText())
-            global_name = 'GeneratedGlobalConstant'  # TODO: This should be extracted from the file name
+            global_name = (
+                "GeneratedGlobalConstant"
+            )  # TODO: This should be extracted from the file name
 
-            return '\n'.join([
-                f"// push static {i}",
-
-                # Load global address
-                f"@{global_name}.{i}",
-                "D=M",
-
-                # *sp = *addr
-                "@SP",  # Fetch the segment pointer
-                "A=M",  # Load *sp into the address register
-                "M=D",  # *sp = *addr
-
-                # Increment stack, sp++
-                "@SP",
-                "M=M+1"
-            ])
-        elif segment == 'temp':
+            return "\n".join(
+                [
+                    f"// push static {i}",
+                    # Load global address
+                    f"@{global_name}.{i}",
+                    "D=M",
+                    # *sp = *addr
+                    "@SP",  # Fetch the segment pointer
+                    "A=M",  # Load *sp into the address register
+                    "M=D",  # *sp = *addr
+                    # Increment stack, sp++
+                    "@SP",
+                    "M=M+1",
+                ]
+            )
+        elif segment == "temp":
             i = int(ctx.INT().getText())
 
             temp_registers_start = 5
@@ -85,27 +86,28 @@ class Visitor(VMVisitor):
             allocated_register = temp_registers_start + i
 
             if allocated_register > temp_registers_end:
-                raise ValueError(f'Unexpected temp register value: "{allocated_register}". This would cause overflow;.'
-                                 f'Available registers: R{temp_registers_start}-R{temp_registers_end}')
+                raise ValueError(
+                    f'Unexpected temp register value: "{allocated_register}". This would cause overflow;.'
+                    f"Available registers: R{temp_registers_start}-R{temp_registers_end}"
+                )
 
-            return '\n'.join([
-                f"// push temp {i}",
-
-                # Calculating addr, where addr = LCL + i
-                f"@{allocated_register}",  # Load the value stored in LCL
-                "D=A",  # D is now addr = temp_registers_start + i
-                "A=D",  # Set address register to addr
-                "D=M",  # Load the value stored within addr, i.e. *addr
-
-                # *sp = *addr
-                "@SP",  # Fetch the segment pointer
-                "A=M",  # Load *sp into the address register
-                "M=D",  # *sp = *addr
-
-                # Increment stack, sp++
-                "@SP",
-                "M=M+1"
-            ])
+            return "\n".join(
+                [
+                    f"// push temp {i}",
+                    # Calculating addr, where addr = LCL + i
+                    f"@{allocated_register}",  # Load the value stored in LCL
+                    "D=A",  # D is now addr = temp_registers_start + i
+                    "A=D",  # Set address register to addr
+                    "D=M",  # Load the value stored within addr, i.e. *addr
+                    # *sp = *addr
+                    "@SP",  # Fetch the segment pointer
+                    "A=M",  # Load *sp into the address register
+                    "M=D",  # *sp = *addr
+                    # Increment stack, sp++
+                    "@SP",
+                    "M=M+1",
+                ]
+            )
         elif segment in hack_segment_names:
             i = ctx.INT().getText()
 
@@ -113,26 +115,25 @@ class Visitor(VMVisitor):
             #   addr = segment + i
             #   *sp = *addr
             #   sp++
-            return '\n'.join([
-                f"// push {segment} {i}",
-
-                # Calculating addr, where addr = LCL + i
-                f"@{hack_segment_names[segment]}",  # Load the value stored in LCL
-                "D=M",
-                f"@{i}",  # Load i
-                "D=D+A",  # D is now addr = LCL + i
-                "A=D",  # Set address register to addr
-                "D=M",  # Load the value stored within addr, i.e. *addr
-
-                # *sp = *addr
-                "@SP",  # Fetch the segment pointer
-                "A=M",  # Load *sp into the address register
-                "M=D",  # *sp = *addr
-
-                # Increment stack, sp++
-                "@SP",
-                "M=M+1"
-            ])
+            return "\n".join(
+                [
+                    f"// push {segment} {i}",
+                    # Calculating addr, where addr = LCL + i
+                    f"@{hack_segment_names[segment]}",  # Load the value stored in LCL
+                    "D=M",
+                    f"@{i}",  # Load i
+                    "D=D+A",  # D is now addr = LCL + i
+                    "A=D",  # Set address register to addr
+                    "D=M",  # Load the value stored within addr, i.e. *addr
+                    # *sp = *addr
+                    "@SP",  # Fetch the segment pointer
+                    "A=M",  # Load *sp into the address register
+                    "M=D",  # *sp = *addr
+                    # Increment stack, sp++
+                    "@SP",
+                    "M=M+1",
+                ]
+            )
         else:
             raise ValueError(f'unexpected segment value: "{segment}"')
 
@@ -142,13 +143,13 @@ class Visitor(VMVisitor):
 
         # Mapping human readable virtual machine names to the hack assembly labels
         hack_segment_names = {
-            'local': 'LCL',
-            'argument': 'ARG',
-            'this': 'THIS',
-            'that': 'THAT'
+            "local": "LCL",
+            "argument": "ARG",
+            "this": "THIS",
+            "that": "THAT",
         }
 
-        if segment == 'temp':
+        if segment == "temp":
             i = int(ctx.INT().getText())
 
             temp_registers_start = 5
@@ -156,45 +157,47 @@ class Visitor(VMVisitor):
             allocated_register = temp_registers_start + i
 
             if allocated_register < temp_registers_start:
-                raise ValueError(f'Unexpected temp register value: "{allocated_register}". This would cause underflow;.'
-                                 f'Available registers: R{temp_registers_start}-R{temp_registers_end}')
+                raise ValueError(
+                    f'Unexpected temp register value: "{allocated_register}". This would cause underflow;.'
+                    f"Available registers: R{temp_registers_start}-R{temp_registers_end}"
+                )
 
-            return '\n'.join([
-                f"// pop temp {i}",
-
-                # Decrement stack, sp--
-                "@SP",
-                "M=M-1",
-
-                # Fetch *SP
-                "@SP",
-                "A=M",
-                "D=M",
-
-                # Store the new value
-                f"@{allocated_register}",
-                "M=D",
-            ])
-        elif segment == 'static':
+            return "\n".join(
+                [
+                    f"// pop temp {i}",
+                    # Decrement stack, sp--
+                    "@SP",
+                    "M=M-1",
+                    # Fetch *SP
+                    "@SP",
+                    "A=M",
+                    "D=M",
+                    # Store the new value
+                    f"@{allocated_register}",
+                    "M=D",
+                ]
+            )
+        elif segment == "static":
             i = int(ctx.INT().getText())
-            global_name = 'GeneratedGlobalConstant'  # TODO: This should be extracted from the file name
+            global_name = (
+                "GeneratedGlobalConstant"
+            )  # TODO: This should be extracted from the file name
 
-            return '\n'.join([
-                f"// pop static {i}",
-
-                # Decrement stack, sp--
-                "@SP",
-                "M=M-1",
-
-                # Fetch *SP
-                "@SP",
-                "A=M",
-                "D=M",
-
-                # Store the new value
-                f"@{global_name}.{i}",
-                "M=D",
-            ])
+            return "\n".join(
+                [
+                    f"// pop static {i}",
+                    # Decrement stack, sp--
+                    "@SP",
+                    "M=M-1",
+                    # Fetch *SP
+                    "@SP",
+                    "A=M",
+                    "D=M",
+                    # Store the new value
+                    f"@{global_name}.{i}",
+                    "M=D",
+                ]
+            )
         elif segment in hack_segment_names:
             i = ctx.INT().getText()
 
@@ -202,74 +205,66 @@ class Visitor(VMVisitor):
             #   sp--
             #   addr = segment + i
             #   *addr = *sp
-            return '\n'.join([
-                f"// pop {segment} {i}",
-
-                # Decrement stack, sp--
-                "@SP",
-                "M=M-1",
-
-                # calculate addr = segment + i, and storing in a temp register R3
-                f"@{hack_segment_names[segment]}",  # Load the value stored in LCL
-                "D=M",
-                f"@{i}",
-                "D=D+A",
-                "@R13",
-                "M=D",
-
-                # D=*sp
-                "@SP",
-                "A=M",
-                "D=M",
-
-                # *addr = *sp
-                "@R13",
-                "A=M",
-                "M=D",
-
-                # Not Needed, but clean up R13:
-                "@R13",
-                "M=0"
-            ])
+            return "\n".join(
+                [
+                    f"// pop {segment} {i}",
+                    # Decrement stack, sp--
+                    "@SP",
+                    "M=M-1",
+                    # calculate addr = segment + i, and storing in a temp register R3
+                    f"@{hack_segment_names[segment]}",  # Load the value stored in LCL
+                    "D=M",
+                    f"@{i}",
+                    "D=D+A",
+                    "@R13",
+                    "M=D",
+                    # D=*sp
+                    "@SP",
+                    "A=M",
+                    "D=M",
+                    # *addr = *sp
+                    "@R13",
+                    "A=M",
+                    "M=D",
+                    # Not Needed, but clean up R13:
+                    "@R13",
+                    "M=0",
+                ]
+            )
         else:
             raise ValueError(f'unexpected segment value: "{segment}"')
 
     # Visit a parse tree produced by VMParser#arithmetic.
     def visitArithmetic(self, ctx: VMParser.ArithmeticContext):
         operation = ctx.getText()
-        if operation == 'add' or operation == 'sub':
-            return '\n'.join([
-                f"// {operation}",
-
-                # Decrement SP to point to the first value
-                "@SP",
-                "M=M-1",
-
-                # Store the "top" value of *SP into the D register
-                "@SP",
-                "A=M",
-                "D=M",
-
-                # Decrement SP to point to the second value
-                "@SP",
-                "M=M-1",
-
-                # Load the second value of *SP for the required arithmetic operation
-                "@SP",
-                "A=M",
-
-                # Perform the required arithmetic operation and store the result in D
-                "D=M+D" if operation == 'add' else "D=M-D",
-
-                # Assign the result of the arithmetic operation to *SP
-                "@SP",
-                "A=M",
-                "M=D",
-
-                # Increment stack, sp++
-                "@SP",
-                "M=M+1"
-            ])
+        if operation == "add" or operation == "sub":
+            return "\n".join(
+                [
+                    f"// {operation}",
+                    # Decrement SP to point to the first value
+                    "@SP",
+                    "M=M-1",
+                    # Store the "top" value of *SP into the D register
+                    "@SP",
+                    "A=M",
+                    "D=M",
+                    # Decrement SP to point to the second value
+                    "@SP",
+                    "M=M-1",
+                    # Load the second value of *SP for the required arithmetic operation
+                    "@SP",
+                    "A=M",
+                    # Perform the required arithmetic operation and store the result in D
+                    "D=M+D" if operation == "add" else "D=M-D",
+                    # Assign the result of the arithmetic operation to *SP
+                    "@SP",
+                    "A=M",
+                    "M=D",
+                    # Increment stack, sp++
+                    "@SP",
+                    "M=M+1",
+                ]
+            )
         else:
             raise ValueError(f'unexpected arithmetic value: "{operation}"')
 
--- test/test_hack.py
+++ test/test_hack.py
@@ -7,152 +7,172 @@ def convert(vm_input):
 
 
 def combine(instructions):
-    return '\n'.join(instructions) + '\n'
+    return "\n".join(instructions) + "\n"
 
 
 def test_push_constant():
     vm_input = "push constant 1337"
-    expected = combine([
-        "// push constant 1337",
-        "@1337",
-        "D=A",
-        "@SP",
-        "A=M",
-        "M=D",
-        "@SP",
-        "M=M+1",
-    ])
+    expected = combine(
+        # fmt: off
+        [
+            "// push constant 1337",
+            "@1337",
+            "D=A",
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1"
+        ]
+        # fmt: on
+    )
 
     assert convert(vm_input) == expected
 
 
 def test_push_local():
     vm_input = "push local 6"
-    expected = combine([
-        "// push local 6",
-        "@LCL",
-        "D=M",
-        "@6",
-        "D=D+A",
-        "A=D",
-        "D=M",
-        "@SP",
-        "A=M",
-        "M=D",
-        "@SP",
-        "M=M+1",
-    ])
+    expected = combine(
+        [
+            "// push local 6",
+            "@LCL",
+            "D=M",
+            "@6",
+            "D=D+A",
+            "A=D",
+            "D=M",
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1",
+        ]
+    )
 
     assert convert(vm_input) == expected
 
 
 def test_push_argument():
     vm_input = "push argument 12"
-    expected = combine([
-        "// push argument 12",
-        "@ARG",
-        "D=M",
-        "@12",
-        "D=D+A",
-        "A=D",
-        "D=M",
-        "@SP",
-        "A=M",
-        "M=D",
-        "@SP",
-        "M=M+1",
-    ])
+    expected = combine(
+        [
+            "// push argument 12",
+            "@ARG",
+            "D=M",
+            "@12",
+            "D=D+A",
+            "A=D",
+            "D=M",
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1",
+        ]
+    )
 
     assert convert(vm_input) == expected
 
 
 def test_push_temp():
     vm_input = "push temp 3"
-    expected = combine([
-        "// push temp 3",
-        "@8",
-        "D=A",
-        "A=D",
-        "D=M",
-        "@SP",
-        "A=M",
-        "M=D",
-        "@SP",
-        "M=M+1",
-    ])
+    expected = combine(
+        [
+            "// push temp 3",
+            "@8",
+            "D=A",
+            "A=D",
+            "D=M",
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1",
+        ]
+    )
 
     assert convert(vm_input) == expected
 
 
 def test_push_static():
     vm_input = "push static 3"
-    expected = combine([
-        "// push static 3",
-        "@GeneratedGlobalConstant.3",
-        "D=M",
-        "@SP",
-        "A=M",
-        "M=D",
-        "@SP",
-        "M=M+1",
-    ])
+    expected = combine(
+        [
+            "// push static 3",
+            "@GeneratedGlobalConstant.3",
+            "D=M",
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1",
+        ]
+    )
 
     assert convert(vm_input) == expected
 
 
 def test_pop_temp():
     vm_input = "pop temp 3"
-    expected = combine([
-        "// pop temp 3",
-        "@SP",
-        "M=M-1",
-        "@SP",
-        "A=M",
-        "D=M",
-        "@8",
-        "M=D",
-    ])
+    expected = combine(
+        # fmt: off
+        [
+            "// pop temp 3",
+            "@SP",
+            "M=M-1",
+            "@SP",
+            "A=M",
+            "D=M",
+            "@8",
+            "M=D"
+        ]
+        # fmt: on
+    )
 
     assert convert(vm_input) == expected
 
 
 def test_pop_local():
     vm_input = "pop local 3"
-    expected = combine([
-        "// pop local 3",
-        "@SP",
-        "M=M-1",
-        "@LCL",
-        "D=M",
-        "@3",
-        "D=D+A",
-        "@R13",
-        "M=D",
-        "@SP",
-        "A=M",
-        "D=M",
-        "@R13",
-        "A=M",
-        "M=D",
-        "@R13",
-        "M=0",
-    ])
+    expected = combine(
+        [
+            "// pop local 3",
+            "@SP",
+            "M=M-1",
+            "@LCL",
+            "D=M",
+            "@3",
+            "D=D+A",
+            "@R13",
+            "M=D",
+            "@SP",
+            "A=M",
+            "D=M",
+            "@R13",
+            "A=M",
+            "M=D",
+            "@R13",
+            "M=0",
+        ]
+    )
 
     assert convert(vm_input) == expected
 
 
 def test_pop_static():
     vm_input = "pop static 3"
-    expected = combine([
-        "// pop static 3",
-        "@SP",
-        "M=M-1",
-        "@SP",
-        "A=M",
-        "D=M",
-        "@GeneratedGlobalConstant.3",
-        "M=D",
-    ])
+    expected = combine(
+        [
+            "// pop static 3",
+            "@SP",
+            "M=M-1",
+            "@SP",
+            "A=M",
+            "D=M",
+            "@GeneratedGlobalConstant.3",
+            "M=D",
+        ]
+    )
 
     print(convert(vm_input))
 
@@ -161,47 +181,51 @@ def test_pop_static():
 
 def test_add():
     vm_input = "add"
-    expected = combine([
-        "// add",
-        "@SP",
-        "M=M-1",
-        "@SP",
-        "A=M",
-        "D=M",
-        "@SP",
-        "M=M-1",
-        "@SP",
-        "A=M",
-        "D=M+D",
-        "@SP",
-        "A=M",
-        "M=D",
-        "@SP",
-        "M=M+1"
-    ])
+    expected = combine(
+        [
+            "// add",
+            "@SP",
+            "M=M-1",
+            "@SP",
+            "A=M",
+            "D=M",
+            "@SP",
+            "M=M-1",
+            "@SP",
+            "A=M",
+            "D=M+D",
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1",
+        ]
+    )
 
     assert convert(vm_input) == expected
 
 
 def test_sub():
     vm_input = "sub"
-    expected = combine([
-        "// sub",
-        "@SP",
-        "M=M-1",
-        "@SP",
-        "A=M",
-        "D=M",
-        "@SP",
-        "M=M-1",
-        "@SP",
-        "A=M",
-        "D=M-D",
-        "@SP",
-        "A=M",
-        "M=D",
-        "@SP",
-        "M=M+1"
-    ])
+    expected = combine(
+        [
+            "// sub",
+            "@SP",
+            "M=M-1",
+            "@SP",
+            "A=M",
+            "D=M",
+            "@SP",
+            "M=M-1",
+            "@SP",
+            "A=M",
+            "D=M-D",
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1",
+        ]
+    )
 
     assert convert(vm_input) == expected
